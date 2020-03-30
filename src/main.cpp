e9130cacd1a4a1095da03e77bc4c2f085c0f898d
makerslet <oshuev.valery@yandex.ru>
2020-03-30 18:22:18 +0300

    Шаблон проекта

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cassert>
+
+/**
+ * @brief Entry point
+ *
+ * Execution of the program
+ * starts here.
+ *
+ * @return Program exit status
+ */
+
+int main (int, char **)
+{
+    return 0;
+}
+
+
